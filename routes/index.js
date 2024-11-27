189a6412773a716f97b272c27aed5e31f3728a9f
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2024-11-27 12:12:08 +0200

    finish movie view app

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ var express = require("express");
 var router = express.Router();
 const request = require("request");
 
+const authorizationHeader = {
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.API_ACCESS_TOKEN}`,
+  },
+};
+
 router.use((req, res, next) => {
   res.locals.imageBaseUrl = process.env.IMAGE_BASE_URL;
   next();
@@ -12,12 +19,7 @@ router.use((req, res, next) => {
 router.get("/", function (req, res, next) {
   request.get(
     process.env.NOW_PLAYING_URL,
-    {
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${process.env.API_ACCESS_TOKEN}`,
-      },
-    },
+    authorizationHeader,
     (error, response, movieData) => {
       const parsedData = JSON.parse(movieData);
       res.render("index", {
@@ -32,17 +34,29 @@ router.get("/movie/:id", (req, res, next) => {
   const thisMovieUrl = `${process.env.API_BASE_URL}/movie/${movieId}`;
   request.get(
     thisMovieUrl,
-    {
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${process.env.API_ACCESS_TOKEN}`,
-      },
-    },
+    authorizationHeader,
     (error, response, movieData) => {
       const parsedData = JSON.parse(movieData);
-      res.render('single-movie', {
-        parsedData
-      })
+      res.render("single-movie", {
+        parsedData,
+      });
+    }
+  );
+});
+
+router.post("/search", (req, res, next) => {
+  const userSearchTerm = encodeURI(req.body.movieSearch);
+  const cat = req.body.cat;
+  const movieUrl = `${process.env.API_BASE_URL}/search/${cat}?query=${userSearchTerm}`;
+  request.get(
+    movieUrl,
+    authorizationHeader,
+    (error, response, searchResults) => {
+      const parsedData = JSON.parse(searchResults);
+      if (cat === "person") {
+        parsedData.results = parsedData.results[0].known_for;
+      }
+      res.render("index", { parsedData: parsedData.results });
     }
   );
 });
