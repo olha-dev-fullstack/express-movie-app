af6412f231ac2348f2612d729f9d7c0c77560f96
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2024-11-27 14:58:25 +0200

    implement own most popular movies route

--- movieApi/routes/index.js
+++ movieApi/routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const movies = require("../data/movies");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/most_popular", (req, res, next) => {
+  let page = req.query.page;
+  if (page === undefined) {
+    page = 1;
+  }
+  // if (req.headers.Authorization.split(" ")[1] !== process.env.API_KEY) {
+  //   res.json("Invalid API Key");
+  // } else {
+  let results = movies.filter((movie) => movie.most_popular);
+  const indexTostart = (page - 1) * 20;
+  results = results.slice(indexTostart, indexTostart + 19);
+  res.json({ results });
+  // }
 });
 
 module.exports = router;
